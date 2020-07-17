8ddfea073cb713f7e1cb1e05aa1df92565b7c775
hyperltech <hyperl.techno@gmail.com>
2020-07-17 09:56:34 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import config from './config';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+import orderRoute from './routes/orderRoute';
+import uploadRoute from './routes/uploadRoute';
+
+
+const cors = require('cors')
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/uploads', uploadRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(config.PAYPAL_CLIENT_ID);
+});
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
+
+const razorpay = new Razorpay({
+  key_id: 'rzp_test_LyQUQA2sH58KIZ',
+  key_secret: 'dygV0PzE4vQX09g6xRMxvwi1'
+})
+
+app.post('/razorpay', async (req, res) => {
+  const payment_capture = 1
+  const amount = 10
+  const currency = 'INR'
+
+  const options = {
+    amount: amount*100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture
+  }
+try{
+  const response = await razorpay.orders.create(options)
+  console.log(response)
+  res.json({
+    id: response.id,
+    amount: response.amount,
+    currency: response.currency
+  })
+}catch(error){
+  console.log(error)
+}
+
+})
+
+
+
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:6000');
+});
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Rating from '../components/Rating';
+
+
+
+function HomeScreen(props) {
+  var save;
+  var myelement;
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+  const category = props.match.params.id ? props.match.params.id : '';
+  const productList = useSelector((state) => state.productList);
+  const { mrp_price, selling_price, products, loading, error } = productList;
+  const discountPrice =  selling_price * mrp_price ;
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(listProducts(category));
+
+    return () => {
+      //
+    };
+  }, [category]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
+  const sortHandler = (e) => {
+    setSortOrder(e.target.value);
+    dispatch(listProducts(category, searchKeyword, sortOrder));
+  };
+
+  return (
+    <>
+      {category && <h2>{category}</h2>}
+
+      <ul className="filter">
+        <li>
+          <form onSubmit={submitHandler}>
+            <input
+              name="searchKeyword"
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </li>
+        <li>
+          Sort By{' '}
+          <select name="sortOrder" onChange={sortHandler}>
+            <option value="">Newest</option>
+            <option value="lowest">Highest</option>
+            <option value="highest">Lowest</option>
+          </select>
+        </li>
+      </ul>
+      <div className="categories">
+      <ul>
+          <li>
+            <img src="https://freshpoint.com/wp-content/uploads/2018/12/root-vegetables-and-tubers-freshpoint-produce-101.jpg" className="vegetables-img"/>
+            <Link to="/category/Root Vegetables" className="category-link">Root Vegetables</Link>
+          </li>
+          <li>
+          <img src="https://imageresizer.static9.net.au/6-BDj1R_NHECVcD5kLEEgX-8bMA=/600x338/smart/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2FNetwork%2FImages%2F2017%2F02%2F14%2F10%2F29%2F170214coach_vegetables.jpg" className="vegetables-img"/>
+          <Link to="/category/Fruit Vegetables" className="category-link">Fruit Vegetables</Link>
+        </li>
+        <li>
+        <img src="https://cdn.britannica.com/s:700x500/17/196817-050-6A15DAC3/vegetables.jpg" className="vegetables-img"/>
+          <Link to="/category/Flower Vegetables" className="category-link">Flower Vegetables</Link>
+        </li>
+        <li>
+        <img src="https://greenmylife-wpengine.netdna-ssl.com/wp-content/uploads/2015/04/Summer-theFruit-season.jpg" className="vegetables-img"/>
+          <Link to="/category/Fruits" className="category-link">Fruits</Link>
+        </li>
+        </ul>
+      </div>
+      {loading ? (
+        <div className="loading"></div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (  
+
+        <ul className="products">
+          {products.map((product) => (
+            <li key={product._id}>
+              <div className="product">
+                <Link to={'/product/' + product._id}>
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                </Link>
+                <noscript>{save= product.mrp_price-product.selling_price}</noscript>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price"><del>&#x20B9;{product.mrp_price}</del>  &#x20B9;{product.selling_price}</div>
+                <div><p className="discount">You save: <b>{Math.round(save/product.mrp_price *100)} %</b></p></div>
+                <div className="product-rating">
+                  <Rating value={product.rating} text={product.numReviews + ' reviews'} />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+export default HomeScreen;
